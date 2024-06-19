2a8948165cf102db738206682edc8bdcd2963513
Uradela <naftali.sumarto.pongilatan-2021@fst.unair.ac.id>
2024-06-19 12:44:29 +0700

    authbaru

--- Auth/UserAuth.js
+++ Auth/UserAuth.js
@@ -54,11 +54,13 @@ router.post('/signup', async (req, res) => {
       email: email,
       password: hashedPassword,
       fullName: username,
-      phone: "",
       gender: "",
       dateOfBirth: "",
       address: "",
       cities: "",
+      weight: "",
+      height: "",
+      age: ""
     };
 
     const newDoc = await usersRef.add(newUser);
@@ -108,3 +110,82 @@ router.post('/login', async (reg, res)=> {
       return res.status(500).json({ error: 'An error occurred during the login process', details: error.message });
     }
   });
+
+// Endpoint to Edit User Profile
+router.post('/edit-profile', async (req, res) => {
+  try {
+    const userId = req.body.uid;
+    const userData = req.body;
+
+    // Validate the required fields
+    const requiredFields = ['email', 'phone', 'fullName', 'gender', 'dateOfBirth', 'address', 'cities', 'weight', 'height', 'age'];
+    for (const field of requiredFields) {
+      if (!userData[field]) {
+        return res.status(400).send(`Field ${field} is required`);
+      }
+    }
+
+    // Reference to the user document
+    const userRef = db.collection('users').doc(userId);
+
+    // Check if the user document exists
+    const userDoc = await userRef.get();
+    if (!userDoc.exists) {
+      return res.status(404).send('User not found');
+    }
+
+    // Update user data in Firestore
+    await userRef.set(userData, { merge: true });
+
+    res.status(200).send('Profile updated successfully');
+  } catch (error) {
+    res.status(500).send('Error updating profile: ' + error.message);
+  }
+});
+
+// Endpoint to reset password
+router.post('/reset-password', async (req, res) => {
+  const { uidLocal, currentPassword, newPassword, confirmNewPassword } = req.body;
+
+  // Validasi input
+  if (!currentPassword || !newPassword || !confirmNewPassword) {
+    return res.status(400).json({ error: 'Please enter all data correctly' });
+  }
+
+  try {
+    if (newPassword !== confirmNewPassword) {
+      return res.status(400).send('New passwords do not match');
+    }
+
+    const userDocRef = db.collection("users").doc(uidLocal);
+    const userDocSnapshot = await userDocRef.get();
+
+    if (!userDocSnapshot.exists) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+
+    const user = userDocSnapshot.data();
+    const userPassword = user.password;
+    const isPasswordValid = await bcrypt.compare(currentPassword, userPassword);
+
+    if (!isPasswordValid) {
+      return res.status(400).send('Current password is incorrect');
+    }
+
+    const isNewPasswordSameAsOld = await bcrypt.compare(newPassword, userPassword);
+
+    if (isNewPasswordSameAsOld) {
+      return res.status(400).send('New password must be different from old password');
+    }
+
+    const hashedNewPassword = await bcrypt.hash(newPassword, 10);
+
+    // Update user password
+    await userDocRef.update({ password: hashedNewPassword });
+    res.status(200).send('Password changed successfully');
+  } catch (error) {
+    res.status(500).send('Error changing password: ' + error.message);
+  }
+});
+
+module.exports = router;
