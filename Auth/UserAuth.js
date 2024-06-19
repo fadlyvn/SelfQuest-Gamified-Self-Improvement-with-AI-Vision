67db2cf83cc9edd0e017b542a150843f9d9f383c
Uradela <naftali.sumarto.pongilatan-2021@fst.unair.ac.id>
2024-06-19 12:27:15 +0700

    regisauth

--- Auth/UserAuth.js
+++ Auth/UserAuth.js
@@ -3,6 +3,77 @@ const bcrypt = require('bcryptjs');
 const db = require('../firebase');
 require('dotenv').config();
 
+const router = express.Router();
+
+// Function to generate random ID
+const generateRandomID = () => {
+  const min = 10000; // Minimal angka acak (4 digit)
+  const max = 99999; // Maksimal angka acak (5 digit)
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+const createCustomID = () => {
+  const prefix = "120"; // Angka yang akan disisipkan di depan ID
+  const randomNumber = generateRandomID();
+  const customID = `${prefix}${randomNumber}`;
+  return customID;
+};
+
+// Endpoint untuk registrasi pengguna baru
+router.post('/signup', async (req, res) => {
+  const { email, username, password, confirmPassword } = req.body;
+
+  // Validasi input
+  if (!email || !username || !password || !confirmPassword) {
+    return res.status(400).json({ error: 'Please enter all data correctly' });
+  }
+
+  if (password !== confirmPassword) {
+    return res.status(400).send('Passwords do not match');
+  }
+
+  try {
+    const usersRef = db.collection('users');
+    const snapshotEmail = await usersRef.where('email', '==', email).get();
+    const snapshotUsername = await usersRef.where('username', '==', username).get();
+
+    if (!snapshotEmail.empty) {
+      return res.status(400).send('Email already in use');
+    }
+
+    if (!snapshotUsername.empty) {
+      return res.status(400).send('Username already in use');
+    }
+
+    const userID = createCustomID().toString();
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = {
+      id: userID,
+      username: username,
+      email: email,
+      password: hashedPassword,
+      fullName: username,
+      phone: "",
+      gender: "",
+      dateOfBirth: "",
+      address: "",
+      cities: "",
+    };
+
+    const newDoc = await usersRef.add(newUser);
+    const uid = newDoc.id;
+    const userDocRef = db.collection('users').doc(uid);
+
+    await userDocRef.set({ ...newUser, uid: uid });
+    res.status(400).send(`Account has been created`);
+  } catch (error) {
+    res.status(500).send('Error creating user: ' + error.message);
+  }
+});
+
+module.exports = router;
+
 
 //Endpoint Login
 router.post('/login', async (reg, res)=> {
